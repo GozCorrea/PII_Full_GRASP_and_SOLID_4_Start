872ae4452ae7f7489eebcdea1a9e19557c56c061
anakaprielian1 <62777141+anakaprielian1@users.noreply.github.com>
2020-05-01 18:05:45 -0300

    se comentan clases y se modifica clase CatalogMaker

---
+++ src/Library/CatalogMaker.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Full_GRASP_And_SOLID
+{
+        //Agregado por patron Creator
+        public class CatalogMaker
+    {
+        private List<Product> productCatalog = new List<Product>();
+        private List<Equipment> equipmentCatalog = new List<Equipment>();
+
+        public void AddProductToCatalog(string description, double unitCost)
+        {
+            productCatalog.Add(new Product(description, unitCost));
+        }
+        public void AddEquipmentToCatalog(string description, double hourlyCost)
+        {
+            equipmentCatalog.Add(new Equipment(description, hourlyCost));
+        }
+        
+        public Product GetProduct(string description)
+        {
+            var query = from Product product in productCatalog where product.Description == description select product;
+            return query.FirstOrDefault();
+        }
+        public Equipment GetEquipment(string description)
+        {
+            var query = from Equipment equipment in equipmentCatalog where equipment.Description == description select equipment;
+            return query.FirstOrDefault();
+        }
+    }
+
+}
